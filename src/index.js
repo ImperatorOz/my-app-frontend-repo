b6c31347f5d8ef536500258033402a6ab2a89836
ImperatorOz <ozbichukwu@gmail.com>
2024-09-23 03:39:02 +0100

    Added new feature: Updated my frontend

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import './App.styles.js'; // Import Global Styles
 import App from './App';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
